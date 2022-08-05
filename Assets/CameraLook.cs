3db6e650b470014d01c17255279ad883d229024c
= <=>
2022-08-05 17:14:58 +0200

    First commit

---
+++ Assets/CameraLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothV;
+
+    [SerializeField] float sensitivity = 5f;
+    [SerializeField] float smoothing = 2f;
+
+    GameObject character;
+
+    void Start()
+    {
+        character = this.transform.parent.gameObject;    
+    }
+
+    void Update()
+    {
+        processRotation();
+    }
+
+    private void processRotation()
+    {
+        var vectorMD = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        smoothV.x = Mathf.Lerp(smoothV.x, vectorMD.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, vectorMD.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+
+    float xPosition, yPosition;
+
+    void Update()
+    {
+        processTranslation();
+    }
+
+    private void processTranslation()
+    {
+        xPosition = speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        yPosition = speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(xPosition, 0f, yPosition);
+    }
+}
